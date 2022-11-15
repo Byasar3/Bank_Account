6941f9e2fbabaf10e41ddfe4f6362527971ca3a9
Berna <btnaberna@Bernas-Macbook-Air.local>
2022-11-15 16:02:26 +0000

    added in private and public bits of properties

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,25 @@
+import java.time.LocalDate;
+
 public class BankAccount {
+
+// PROPERTIES:
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth; // = LocalDate.of(2022,04,11)
+    private Integer accountNumber;
+    private Double balance;
+
+// making properties
+
+    public BankAccount (String inputFirstName, String inputLastName, LocalDate inputDOB,
+                        Integer inputAccountNumber, Double inputBalance){
+        this.firstName = inputFirstName;
+        this.lastName = inputLastName;
+        this.dateOfBirth = inputDOB;
+        this.accountNumber = inputAccountNumber;
+        this.balance = inputBalance;
+
+    }
+
 }
